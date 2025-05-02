9eb5f8c299139e6d5feb583b5bb46bdc98dbda1c
Sachin Jadhav <139002222+SACHIN9637@users.noreply.github.com>
2025-05-02 17:19:21 +0530

    Delete Network-Intrusion-Detector/IDS/RAG_pipeline directory

--- Network-Intrusion-Detector/IDS/RAG_pipeline/chatbot.py
+++
@@ -1,9 +0,0 @@
-import ollama
-a = input("Ask a Question")
-response = ollama.chat(model='llama3.2', messages=[
-  {
-    'role': 'user',
-    'content': a,
-  },
-])
-print(response['message']['content'])
